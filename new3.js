47d5ea5edf321c4d08c6397c1346164218549bf7
Ankit Kumar <ankit@192.168.1.37>
2026-02-06 13:06:38 +0530

    added file

---
+++ new3.js
@@ -0,0 +1,8 @@
+const user = {
+  firstName: "John",
+  lastName: "Doe"
+};
+
+console.log("First Name:", user.firstName);
+console.log("Last Name:", user.lastName);
+console.log("Full Name:", user.firstName + " " + user.lastName);
