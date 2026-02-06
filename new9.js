4e661883d48de15f57ead28b3a8a7f8b7d089677
Ankit Kumar <ankit@192.168.1.37>
2026-02-06 13:25:19 +0530

    test11

---
+++ new9.js
@@ -0,0 +1,13 @@
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  address: "123 Main Street",
+  pinCode: "560001",
+  dob: "111"
+};
+
+console.log("First Name:", user.firstName);
+console.log("Last Name:", user.lastName);
+console.log("Address:", user.address);
+console.log("Pin Code:", user.pinCode);
+console.log("Full Name:", user.firstName + " " + user.lastName);
